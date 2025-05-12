8cf4d4e47bad09db4ebba54e9567df87cc599515
GerardoNR <22620048@tlaxiaco.tecnm.mx>
2025-05-12 09:11:59 -0600

    Act6-Crenado metodo Put en el controlador

--- src/main/java/com/noe/cafeteria/controllers/cafeteriaController.java
+++ src/main/java/com/noe/cafeteria/controllers/cafeteriaController.java
@@ -1,8 +1,13 @@
 package com.noe.cafeteria.controllers;
 import java.util.List;
+
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,4 +32,18 @@ public class cafeteriaController {
 public cafeteria insertarCafeteria(@RequestBody cafeteria cliente) {
     return cafeteriaRepository.save(cliente);
 }
+//metodo para editar un cliente en la base de datos
+@PutMapping ("/editar-cliente/{id}") 
+public ResponseEntity<cafeteria> actualizarcliente (@PathVariable Long id, @RequestBody cafeteria cliente) {
+return cafeteriaRepository.findById(id).map(clienteExiste ->{
+    clienteExiste.setNombre(cliente.getNombre());
+    clienteExiste.setApellido(cliente.getApellido());
+    clienteExiste.setTelefono(cliente.getTelefono());
+    clienteExiste.setEmail(cliente.getEmail());
+    clienteExiste.setImagenURL(cliente.getImagenURL());
+    cafeteria actualizado = cafeteriaRepository.save(clienteExiste);
+    return ResponseEntity.ok(actualizado);
+}).orElse(ResponseEntity.notFound().build());
 }
+}
+
