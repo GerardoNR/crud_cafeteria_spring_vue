9c8cf957823a03784c918f94399f25d3d94639f7
GerardoNR <22620048@tlaxiaco.tecnm.mx>
2025-05-12 08:14:22 -0600

    Act3-Creando Repository para cafeteria

---
+++ src/main/java/com/noe/cafeteria/repository/cafeteriaRepository.java
@@ -0,0 +1,9 @@
+package com.noe.cafeteria.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.noe.cafeteria.model.cafeteria;
+
+public interface cafeteriaRepository extends JpaRepository <cafeteria,Long> {
+
+}
