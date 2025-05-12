66647b8b7b9c64efcec1fc35b863a4060845f2e4
GerardoNR <22620048@tlaxiaco.tecnm.mx>
2025-05-12 08:40:21 -0600

    Act5-Creando metodo Pust en el controlador

--- src/main/java/com/noe/cafeteria/controllers/cafeteriaController.java
+++ src/main/java/com/noe/cafeteria/controllers/cafeteriaController.java
@@ -2,6 +2,8 @@ package com.noe.cafeteria.controllers;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +17,14 @@ public class cafeteriaController {
     @Autowired
     private cafeteriaRepository cafeteriaRepository;
 
+// metodo get para mostrar clientes
     @GetMapping("/mostar-cafeteria")
     public List<cafeteria> mostrarCafeteria () {
         return cafeteriaRepository.findAll();
     }
-
+//metodo para insertar un cliente a la BD
+@PostMapping("/insertar-cliente")
+public cafeteria insertarCafeteria(@RequestBody cafeteria cliente) {
+    return cafeteriaRepository.save(cliente);
+}
 }
